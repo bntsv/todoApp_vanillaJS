8f38763c572f8230ab06da48575d68d88611504c
bntsv <you@example.com>
2021-08-23 00:33:00 +0300

    ADD REMOVE FETCH - NO JSON Server

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,81 +1,77 @@
+let todosArr = [];
+let apiUrl = `https://jsonplaceholder.typicode.com/todos`;
+
+function renderTodos(todosArr) {
+    domEl.ul.innerHTML = ``;
+    todosArr.forEach( todo =>{
+        let li = document.createElement("li");
+
+        li.setAttribute('data-id', `${todo.id}`); 
+        li.innerHTML = `
+        <span class="${todo.completed?'completed':''}">${todo.title}</span>
+        <div class="todo-remove-btn" id = ${todo.id}><i class="far fa-trash-alt"></i></div>
+        `;
+    
+        domEl.ul.appendChild(li);
+        li.addEventListener("click", removeTodo);
+    })
+    domEl.total.innerHTML= `${todosArr.length}`;
+}
+
+function addTodo(){
+    
+    let todoTitle = domEl.userInput.value;
+
+    if(todoTitle !== ""){
+        let todo = {
+            title: todoTitle,
+            completed: false
+        };
+    
+        todosArr.push(todo)
+        todo.id = (todosArr.indexOf(todo))+1;
+    }
+
+    domEl.userInput.value = "";
+    domEl.userInput.focus();
+    renderTodos(todosArr);
+}
+
+function removeTodo (evt){
+
+    console.log(evt.currentTarget);
+    console.log(evt.target);
+
+    const liNode = evt.currentTarget;
+	const todoID = +liNode.dataset.id;
+	const removeBtnID = +evt.target.parentNode.id;
+    
+
+    if( removeBtnID === todoID){
+
+        console.log(`remove`);
+        let idx = todosArr.findIndex( todo => todo.id === todoID );
+        console.log(idx);
+		idx>=0 && todosArr.splice(idx,1);
+        
+        renderTodos(todosArr);
+    }
+    
+}
+
+fetch(`${apiUrl}`)
+	.then( response => response.json() )
+	.then( data => {
+		todosArr = [...todosArr,...data];
+		renderTodos(todosArr)
+	} )
+	.catch(err => console.error(err))
+
 let domEl = {
-    // todoTitle : document.querySelector(".todo-add > input").value,
+    userInput : document.querySelector(".todo-add > input"),
     addBtn : document.querySelector(".todo-add-btn"),
     ul : document.querySelector(".todo-items"),
     total : document.querySelector(".output")
 }
 
-let todosArr = [];
-
-class TodoObj {
-    constructor(){
-        this.title;
-        this.id;
-    }
-}
-
-TodoObj.prototype.removeTodo = function (evt){
-    
-    console.log(evt.currentTarget.id);
-    console.log(evt.target.id);
-
-    if(evt.target.id === evt.currentTarget.id){
-
-        console.log(`remove`);
-
-
-        // todosArr[evt.target.id] = {};
-            
-        // todosArr.length--;
-        // domEl.total.innerHTML= `${todosArr.length}`;
-    }
-    
-}
-
-function addTodo(){
-    
-    let todo = new TodoObj();
-    todo.title = document.querySelector(".todo-add > input").value;
-    todosArr.push(todo)
-    todo.id = (todosArr.indexOf(todo))+1;
-
-    console.log(todo);
-
-    let li = document.createElement("li");
-    // li.setAttribute('data-id', `${todo.id}`); 
-    li.innerHTML = `<span>${todo.id}. ${todo.title}</span><div class="todo-remove-btn" id = ${todo.id}><i class="far fa-trash-alt"></i></div>`;
-
-    domEl.ul.appendChild(li);
-
-    document.querySelector(".todo-add > input").value = "";
-    domEl.total.innerHTML= `${todosArr.length}`;
-
-    return todo;
-}
-
 domEl.addBtn.addEventListener("click", addTodo);
-
-
-if (todosArr.length>0){
-    
-    // let li = document.querySelector(".todo-items>li");
-
-    // function removeTodo(evt){
-    //     console.log(evt.currentTarget.id);
-    //     console.log(evt.target.id);
-
-    //     if(evt.target.id === evt.currentTarget.id){
-
-    //         console.log(`remove`);
-
-
-    //         todosArr[evt.target.id] = {};
-            
-    //         todosArr.length--;
-    //         domEl.total.innerHTML= `${todosArr.length}`;
-    //     }
-    
-    // }
-    let removeBtn = document.querySelector(".todo-remove-btn");
-    removeBtn.addEventListener("click", addTodo().removeTodo);
-}
