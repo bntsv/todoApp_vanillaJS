979a0830686bce10c5977e69d273d0013a0317cf
bntsv <you@example.com>
2021-08-29 23:10:39 +0300

    done with REST Api / json-server

---
+++ Readme.md
@@ -0,0 +1,14 @@
+Simple Todo App
+Created with HTML, CSS and Vanilla JavaScript.
+
+How to start
+### clone the repo to local folder:
+git clone https://github.com/bntsv/todoApp_vanillaJS
+
+### navigate to root project folder:
+cd todoApp_vanillaJS
+
+### install dependencies
+npm install
+
+Then start the json-server and live-server.
--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,8 +1,6 @@
-let todosArr = [];
-let apiUrl = `https://jsonplaceholder.typicode.com/todos`;
-
 function renderTodos(todosArr) {
     domEl.ul.innerHTML = ``;
+    
     todosArr.forEach( todo =>{
         let li = document.createElement("li");
 
@@ -11,7 +9,7 @@ function renderTodos(todosArr) {
         <span class="${todo.completed?'completed':''}">${todo.title}</span>
         <div class="todo-remove-btn" id = ${todo.id}><i class="far fa-trash-alt"></i></div>
         `;
-    
+        
         domEl.ul.appendChild(li);
         li.addEventListener("click", removeTodo);
     })
@@ -21,51 +19,82 @@ function renderTodos(todosArr) {
 function addTodo(){
     
     let todoTitle = domEl.userInput.value;
+    let todo = {
+        title: todoTitle,
+        completed: false
+    };
+    // todo.id = (todosArr.indexOf(todo))+1;
 
     if(todoTitle !== ""){
-        let todo = {
-            title: todoTitle,
-            completed: false
-        };
-    
-        todosArr.push(todo)
-        todo.id = (todosArr.indexOf(todo))+1;
+        
+        //change server(db) state:
+        fetch(`${apiRoot}/todos`, {
+                method: "POST",
+                headers: {
+                    'Content-Type':'application/json'
+                },
+                body: JSON.stringify(todo)
+                })
+            .then((response)=>response.json())
+            .then(todo => {
+                //change local state:
+                todosArr = [...todosArr, todo];
+                renderTodos(todosArr);
+                
+            })
+            .catch(err => console.error(err))
     }
-
+    
     domEl.userInput.value = "";
     domEl.userInput.focus();
-    renderTodos(todosArr);
 }
 
 function removeTodo (evt){
 
-    console.log(evt.currentTarget);
-    console.log(evt.target);
+    // console.log(evt.currentTarget);
+    // console.log(evt.target);
 
     const liNode = evt.currentTarget;
 	const todoID = +liNode.dataset.id;
-	const removeBtnID = +evt.target.parentNode.id;
+	// const removeBtnID = +evt.target.parentNode.id;
     
+    //change server(db) state:
 
-    if( removeBtnID === todoID){
+    fetch(`${apiRoot}/todos/${todoID}`, {
+        method: "DELETE"
+    })
+        .then((response)=>{
 
-        console.log(`remove`);
-        let idx = todosArr.findIndex( todo => todo.id === todoID );
-        console.log(idx);
-		idx>=0 && todosArr.splice(idx,1);
-        
-        renderTodos(todosArr);
-    }
+            //change local state:
     
+            if( response.status === 200){
+                let idx = todosArr.findIndex( todo=> todo.id === todoID);
+                idx >=0 && todosArr.splice(idx,1);
+                renderTodos(todosArr) 
+            }
+        })
+        .catch(err => console.error(err))
 }
 
-fetch(`${apiUrl}`)
-	.then( response => response.json() )
-	.then( data => {
-		todosArr = [...todosArr,...data];
-		renderTodos(todosArr)
-	} )
-	.catch(err => console.error(err))
+let fetchTodos = function(apiRoot){
+    fetch(`${apiRoot}/todos`)
+        .then((response)=>response.json())
+        .then( data=> {
+            todosArr = data;
+            renderTodos(todosArr)
+        } )
+        .catch(err => console.error(err))
+}
+
+// let apiUrl = `https://jsonplaceholder.typicode.com/todos`;
+
+// fetch(`${apiUrl}`)
+// 	.then( response => response.json() )
+// 	.then( data => {
+// 		todosArr = [...todosArr,...data];
+// 		renderTodos(todosArr)
+// 	} )
+// 	.catch(err => console.error(err))
 
 let domEl = {
     userInput : document.querySelector(".todo-add > input"),
@@ -73,5 +102,11 @@ let domEl = {
     ul : document.querySelector(".todo-items"),
     total : document.querySelector(".output")
 }
+let todosArr = [];
+let apiRoot = `http://localhost:3000`;
+
+window.addEventListener(`DOMContentLoaded`, function(){
+    fetchTodos(apiRoot);
+});
 
 domEl.addBtn.addEventListener("click", addTodo);
