367d2931c43478a4375b239cf1789a4e1c1ae0e8
bntsv <you@example.com>
2021-09-04 16:45:01 +0300

    id bug fixed, event handling fixed

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,77 +1,100 @@
-let todosArr = [];
-let apiUrl = `https://jsonplaceholder.typicode.com/todos`;
-
 function renderTodos(todosArr) {
     domEl.ul.innerHTML = ``;
-    todosArr.forEach( todo =>{
+    todosArr.forEach(todo => {
         let li = document.createElement("li");
 
-        li.setAttribute('data-id', `${todo.id}`); 
+        li.setAttribute('data-id', `${todo.id}`);
         li.innerHTML = `
-        <span class="${todo.completed?'completed':''}">${todo.title}</span>
+        <span class="${todo.completed ? 'completed' : ''}">${todo.title}</span>
         <div class="todo-remove-btn" id = ${todo.id}><i class="far fa-trash-alt"></i></div>
+        <div class="editTodo"><i class="fas fa-check"></i></div>
         `;
-    
+
         domEl.ul.appendChild(li);
         li.addEventListener("click", removeTodo);
+        li.addEventListener("click", toggleCompleted);
     })
-    domEl.total.innerHTML= `${todosArr.length}`;
+    domEl.total.innerHTML = `${todosArr.length}`;
 }
 
-function addTodo(){
-    
-    let todoTitle = domEl.userInput.value;
+function addTodo() {
 
-    if(todoTitle !== ""){
-        let todo = {
-            title: todoTitle,
-            completed: false
-        };
-    
-        todosArr.push(todo)
-        todo.id = (todosArr.indexOf(todo))+1;
-    }
+    let todoTitle = domEl.userInput.value;
+    if (!todoTitle) return;
+
+    let currentLastTodoID = todosArr[todosArr.length-1].id;
+
+    let todo = {
+        id: currentLastTodoID+1,
+        title: todoTitle,
+        completed: false
+    };
+
+    console.log(todo.id);
+    todosArr.push(todo)
 
     domEl.userInput.value = "";
     domEl.userInput.focus();
     renderTodos(todosArr);
 }
 
-function removeTodo (evt){
+function removeTodo(evt) {
 
     console.log(evt.currentTarget);
     console.log(evt.target);
 
     const liNode = evt.currentTarget;
-	const todoID = +liNode.dataset.id;
-	const removeBtnID = +evt.target.parentNode.id;
-    
+    const todoID = +liNode.dataset.id;
+    const removeBtnID = +evt.target.parentNode.id;
 
-    if( removeBtnID === todoID){
+    // if({evt.target.classList.contains(`fas`, `fa-check`)){
 
-        console.log(`remove`);
-        let idx = todosArr.findIndex( todo => todo.id === todoID );
-        console.log(idx);
-		idx>=0 && todosArr.splice(idx,1);
-        
+    // }
+    if (removeBtnID === todoID) {
+
+        // console.log(`remove`);
+        let idx = todosArr.findIndex(todo => todo.id === todoID);
+        // console.log(idx);
+        idx >= 0 && todosArr.splice(idx, 1);
+
         renderTodos(todosArr);
     }
-    
 }
 
-fetch(`${apiUrl}`)
-	.then( response => response.json() )
-	.then( data => {
-		todosArr = [...todosArr,...data];
-		renderTodos(todosArr)
-	} )
-	.catch(err => console.error(err))
+function toggleCompleted(evt) {
+
+    console.log(evt.currentTarget);
+    console.log(evt.target);
+
+    const liNode = evt.currentTarget;
+    const todoID = +liNode.dataset.id;
+
+    if(evt.target.classList.contains(`fas`, `fa-check`)){
+
+        let idx = todosArr.findIndex(todo => todo.id === todoID);
+        todosArr[idx].completed = !todosArr[idx].completed;
+
+        renderTodos(todosArr);
+    }
+}
+
+let apiUrl = `https://jsonplaceholder.typicode.com/todos`;
+(function(){
+    fetch(`${apiUrl}`)
+    .then(response => response.json())
+    .then(data => {
+        todosArr = [...todosArr, ...data];
+        renderTodos(todosArr)
+    })
+    .catch(err => console.error(err))
+})();
 
 let domEl = {
-    userInput : document.querySelector(".todo-add > input"),
-    addBtn : document.querySelector(".todo-add-btn"),
-    ul : document.querySelector(".todo-items"),
-    total : document.querySelector(".output")
+    userInput: document.querySelector(".todo-add > input"),
+    addBtn: document.querySelector(".todo-add-btn"),
+    ul: document.querySelector(".todo-items"),
+    total: document.querySelector(".output")
 }
+let todosArr = [];
 
 domEl.addBtn.addEventListener("click", addTodo);
